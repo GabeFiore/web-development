27362432e90f8a7607a49a6c03883bf52c5b3429
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:34:02 -0300

    Atualizando

---
+++ manipulando_textos.js
@@ -0,0 +1,7 @@
+//substitui um trecho do texto
+let txt = "Estão chegando as provas";
+console.log(txt.replace("provas", "avalicações"));
+console.log(txt.toUpperCase());
+//define o numero de casas decimais 
+let num = 123.456;
+console.log(num.toFixed(1));
---
+++ operadores.js
@@ -0,0 +1,11 @@
+let num = 22;
+let num1 = 8;
+//operações
+let soma = num + num1 
+let subtração = num - num1 
+let multiplicacao = num * num1 
+let divisao = num / num1 
+console.log("O resultado da soma é: " ,soma);
+console.log("O resultado da subtração é: " ,subtração);
+console.log("O resultado da multiplição é: " ,multiplicacao);
+console.log("O resultado da divisão é: " ,divisao);
