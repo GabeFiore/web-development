0c964e9b3d0574e761d728865425db74a8b766c3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 09:09:35 -0300

    ()

---
+++ if-else.js
@@ -0,0 +1,27 @@
+//if - else 
+let valor = 6;
+if (valor > 8){
+    console.log("O valor é maior que 8");
+} else {
+    console.log("O valor é menor que 8");
+}
+
+
+let idade = 30
+if (idade <= 12) {
+    console.log("É uma criança");
+} else if (idade <= 17) {
+    console.log("É um adolescente");
+} else if (idade <= 29) {
+    console.log("É um jovem");
+} else {
+    console.log("É um adulto");
+}
+
+// && para E - || para OU
+let valor1 = 18;
+if (valor1 > 8 || valor1 <20){
+    console.log("O valor é válido");
+} else {
+    console.log("O valor é inválido");
+}
--- ternario.js
+++ ternario.js
@@ -1,6 +1,4 @@
 //numero par ou impar
-let number = 7;
-let resultado = (number % 2 === 0) ? "par" : "impar";
+let number = 8;
+let resultado = (number % 2 === 0) ? "Par" : "Impar";
 console.log(resultado);
-
-//
