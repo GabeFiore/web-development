df611005ff97c817d469d8fb9077f948ec8b6ec0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 09:51:22 -0300

    #

---
+++ array.js
@@ -0,0 +1,17 @@
+const times = ["Corinthians", "Timão", "Racing", "Barcelona"]
+
+console.table(times)
+
+times.push("Inter Miami")
+
+console.table(times)
+
+console.log()//exercicio
+
+const cidades = ["São Paulo", "Rio de Janeiro", "Minas Gerais"];
+let upperCaseCidades = []
+for (let upperCaseCidades of cidades){
+    console.log(upperCaseCidades);
+}
+
+
---
+++ while.js
@@ -0,0 +1,28 @@
+let i = 0;
+
+while(i<=2){
+    console.log(i);
+    i++
+}
+
+console.log() //exercicio 1
+
+let n = 0;
+while(n<=5){
+    console.log(n);
+    n++
+}
+
+console.log() //exercicio 2
+
+/*
+let x = 1; 
+while(x <= 10){
+    console.log(`É a ${[x]}a vez que esta frase é exibida no console`);
+    x++
+}
+*/
+
+for(let x = 1; x <=10; x++){
+    console.log(`É a ${x}a vez que esta frase é exibida no console`)
+}
