7737760d1093f6e9819148f4fb54a324c581304a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 09:56:19 -0300

    $

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,26 @@
-function teste(agr1, agr2){
+/*function teste(agr1, agr2){
     console.log(agr1 + agr2);
 }
-
 teste(7, 1);
 
 function imprimeNome(name){
     console.log(`Meu nome é ${name}`)
 }
-
 imprimeNome("Gabriel");
 imprimeNome("Mike");
+
+function cliqueAqui(){
+    alert("Óla")
+}*/
+
+//CRIANDO VARIÁVEL
+const tirarDiv = document.querySelector(".tirarDiv");
+const voltarDiv = document.querySelector(".voltarDiv");
+const card = document.querySelector(".card");
+
+tirarDiv.addEventListener("click", function(){
+    card.style.display = "none";
+});
+voltarDiv.addEventListener("click", function(){
+    card.style.display = "block";
+});
