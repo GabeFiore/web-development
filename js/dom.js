f6bbe9ef9564e69f68b6802f9113f5b9a548476d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:50:06 -0300

    $

---
+++ js/dom.js
@@ -0,0 +1,12 @@
+function teste(agr1, agr2){
+    console.log(agr1 + agr2);
+}
+
+teste(7, 1);
+
+function imprimeNome(name){
+    console.log(`Meu nome é ${name}`)
+}
+
+imprimeNome("Gabriel");
+imprimeNome("Mike");
